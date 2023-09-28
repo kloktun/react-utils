cddc056b9417065694832d8152d8360c7dcbbac5
Alexey Kloktunov <kloktun@gmail.com>
2023-09-28 23:25:16 +0400

    chore: add imports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { useLoading } from './hooks/useLoading';
 export { useListLoad } from './hooks/useListLoad';
 export { usePasswordGenerator } from './hooks/usePasswordGenerator';
 export { useCopyToClipboard } from './hooks/useCopyToClipboard';
+export { useClickOutside } from './hooks/useClickOutside';
+export { useCombinedRefs } from './hooks/useCombinedRefs';
