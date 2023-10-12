435b30f503dfd6e3e5881062a054e596d895872c
Alexey Kloktunov <kloktun@gmail.com>
2023-10-12 14:37:23 +0400

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,6 @@ export { usePasswordGenerator } from './hooks/usePasswordGenerator';
 export { useCopyToClipboard } from './hooks/useCopyToClipboard';
 export { useClickOutside } from './hooks/useClickOutside';
 export { useCombinedRefs } from './hooks/useCombinedRefs';
+export { useLoad } from './hooks/useLoad';
+export { useUnloadConfirm } from './hooks/useUnloadConfirm';
+export { useBoolean } from './hooks/useBoolean';
